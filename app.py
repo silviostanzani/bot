9689bf9c35cfe3d622162ee3eda49b7f1dee7324
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2021-03-23 15:31:31 -0300

    template

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#import os
 import flask
 import json
 from flask import Flask,request, jsonify
@@ -10,33 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
+
 def index():
     return render_template('index.html')
-
-
-
-#@app.route('/zipcode/<zipcode>') # http://localhost:5000/zipcode/60050111
-#def validar_cep(zipcode):
-#    '''Função valida de número de cep junto ao site dos Correios.
-#        - Parâmetro: cep.
-#        - Retorna: json com as infomações:
-#            - Cep válidos: Logradouro/Nome; Bairro/Distrito; Localidade/UF; CEP.
-#            - Inválidos: msn: "Cep inválido"
-#    '''
-#    try:
-#        endereco = pycep_correios.get_address_from_cep(zipcode)
-#        #exemplo: http://localhost:5000/Cep/60050220
-#        res = endereco
-#        return json.dumps(res, ensure_ascii=False).encode('utf8')
-#        #return jsonify(endereco)#f'<h1>Cep {endereco}</h1>'
-#    except:
-#        res = f'zip code invalid.:{zipcode}.'
-#        return json.dumps(res, ensure_ascii=False).encode('utf8')
-#        #return jsonify(f'Cep inválido:{cep}.')
-
-##teste localhost
-#'''if __name__ == '__main__':
-#    app.run(debug=True)'''#
 
 ## É necessário um token, obtido atráves do BotFather do próprio Telegram
 token = "1720866154:AAF9dw56n4wgn4JW-eKCh5Ni0-VMjWUgFPs"
@@ -75,12 +50,7 @@ bot.polling()
 
 # main - flask
 #teste localhost
-'''if __name__ == '__main__':
-    bot.polling()	
-    app.run(debug=True)'''
-
-#teste heroku
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host='0.0.0.0', port=port)
+#'''if __name__ == '__main__':
+#    bot.polling()	
+#    app.run(debug=True)'''
 
